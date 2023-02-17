b979723db722ab495743870a4e6e0e89e98d6261
Dmitry220 <dmitry19.kravtsov123@gmail.com>
2023-02-17 18:22:53 +0300

    second

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -6,9 +6,9 @@ export const Modal = ({active, setActive, children}) => {
 
   return (
 	  <div className={active ? "modal active" : "modal"} onClick={()=>setActive(false)}>
-		  <div className={active ? "modal__content active" : "modal__content"} onClick={e=>e.stopPropagation()}>
+		  {/* <div className={active ? "modal__content active" : "modal__content"} onClick={e=>e.stopPropagation()}>
 				{children}
-		  </div>
+		  </div> */}
 	  </div>
   );
 };
